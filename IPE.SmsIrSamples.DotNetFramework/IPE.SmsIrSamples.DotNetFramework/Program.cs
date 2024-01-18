6aaa4f12cc0e5926c1825488f50268791ae6b63d
soroushkavousi <soroushkavousi.me@gmail.com>
2024-01-18 10:02:17 +0330

    chore: initialize project

---
+++ IPE.SmsIrSamples.DotNetFramework/IPE.SmsIrSamples.DotNetFramework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IPE.SmsIrSamples.DotNetFramework
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
